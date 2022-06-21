c24e4343082736ab7c326613bf4e15c51a969239
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-21 21:53:41 +0900

    Add functionality, choose player rock.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 let win = 1;
 let lose = 0;
 
-let humanPlayerChoose = [rock, paper, scissors];
-let computerPlayerChoose = [ROCK, PAPER, SCISSORS];
+let humanPlayerChoose = ["rock", "paper", "scissors"];
 
-function computerPlayerChoose() {
+const buttonTest = document.querySelector("#test-button");
+function computerPlayer() {
+    let computerPlayerChoose = ["ROCK", "PAPER", "SCISSORS"];
+    let randomComNum = Math.floor(Math.random()*computerPlayerChoose.length);
+    let randomComArray = computerPlayerChoose[randomComNum];
+    console.log(randomComArray);
+}
+buttonTest.addEventListener('click', computerPlayer);
+
+
+const buttonRock = document.querySelector("#button-rock");
+const buttonPaper = document.querySelector("#button-paper");
+const buttonScissors = document.querySelector("#button-scissors");
+
+buttonRock.addEventListener('click', alert(humanPlayerChoose[0]));
+
+function playGame(playerSelection, computerSelection) {
 
 }
