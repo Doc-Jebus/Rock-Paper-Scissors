2d3b0636a5e703a7ed10300c7fa34b0e6bc6c2f3
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-06-22 13:07:02 +0900

    Started main function playGame()

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 let win = 1;
 let lose = 0;
 
-let humanPlayerChoose = ["rock", "paper", "scissors"];
+let humanPlayer = ["rock", "paper", "scissors"];
+let computerPlayer = ["ROCK", "PAPER", "SCISSORS"];
 
 const buttonTest = document.querySelector("#test-button");
 function computerPlayer() {
-    let computerPlayerChoose = ["ROCK", "PAPER", "SCISSORS"];
-    let randomComNum = Math.floor(Math.random()*computerPlayerChoose.length);
-    let randomComArray = computerPlayerChoose[randomComNum];
+    
+    let randomComNum = Math.floor(Math.random()*computerPlayer.length);
+    let randomComArray = computerPlayer[randomComNum];
     console.log(randomComArray);
 }
 buttonTest.addEventListener('click', computerPlayer);
 
 
-const buttonRock = document.querySelector("#button-rock");
+const buttonRock = document.querySelector("#button-rock").value(humanPlayer[0]);
 const buttonPaper = document.querySelector("#button-paper");
 const buttonScissors = document.querySelector("#button-scissors");
 
-buttonRock.addEventListener('click', alert(humanPlayerChoose[0]));
-buttonPaper.addEventListener('click', alert(humanPlayerChoose[1]));
-buttonScissors.addEventListener('click', alert(humanPlayerChoose[2]));
-
 function playGame(playerSelection, computerSelection) {
-
+    let playerSelection = humanPlayer[buttonRock, buttonPaper, buttonScissors];
+    let computerSelection = computerPlayer();
+    alert(playerSelection, computerSelection);
 }
+    
+buttonRock.addEventListener('click', playGame);
+buttonPaper.addEventListener('click', playGame);
+buttonScissors.addEventListener('click', playGame);
+
